09e46dd4de1493b10665d74e8524e5b0c5f4c739
Antoine Roucau <antoine.roucau@gmail.com>
2025-04-25 11:22:14 +0200

    Hierachie

--- obj/Debug/net8.0/TransConnect.AssemblyInfo.cs
+++ obj/Debug/net8.0/TransConnect.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TransConnect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+217c5ae6ac9462989ff6119a1f140586011e3bf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64220e34ade7b6d55f3a96404a34e3b7b432798a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TransConnect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TransConnect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
