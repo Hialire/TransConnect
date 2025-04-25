18aec7cbf004aa81a9b441da6388c1a572ec09c2
max3000aez <maxime.mottier@edu.devinci.fr>
2025-04-25 18:27:57 +0200

    modification dataframe

--- Services/ClientService.cs
+++ Services/ClientService.cs
@@ -1,40 +1,58 @@
 using System;
 using System.Data;
+using System.Collections.Generic; // pour List<Client>
 using TransConnect.Models;
+
 namespace Transconnect.Services
 {
     public class ClientService : Personne
     {
         private decimal montantAchatsCumulés;
 
-        public ClientService(string numeroSS, string nom, string prenom, DateTime dateNaissance, string adressePostale, string adresseEmail, string telephone, decimal montantAchats)
+        public ClientService(string numeroSS, string nom, string prenom, DateTime dateNaissance,
+            string adressePostale, string adresseEmail, string telephone, decimal montantAchats)
             : base(numeroSS, nom, prenom, dateNaissance, adressePostale, adresseEmail, telephone)
         {
             this.montantAchatsCumulés = montantAchats;
         }
+
         public decimal MontantAchatsCumulés
         {
             get { return montantAchatsCumulés; }
             set { if (value >= 0) montantAchatsCumulés = value; }
         }
 
-        DataTable dfClient = new DataTable();
-        dfClient.Columns.Add("Numéro de Sécurité Sociale", typeof(string));
-        dfClient.Columns.Add("Nom", typeof(string));
-        dfClient.Columns.Add("Prénom", typeof(string));
-        dfClient.Columns.Add("Date de Naissance", typeof(DateTime));
-        dfClient.Columns.Add("Adresse Postale", typeof(string));
-        dfClient.Columns.Add("Adresse Email", typeof(string));
-        dfClient.Columns.Add("Téléphone", typeof(string));
-        dfClient.Columns.Add("Montant Achats Cumulés", typeof(decimal));
-
-        foreach(Client c in ClientList)
+        public DataTable GetClientsTriés(List<Client> ClientList)
         {
-            dfClient.Rows.Add(c.NumeroSS, c.Nom, c.Prenom, c.DateNaissance, c.AdressePostale, c.AdresseEmail, c.Telephone);
-        }
-        DataView vue = dfClient.DefaultView;
-        vue.Sort = "Nom";
-        DataTable dfClientTrie = vue.ToTable();
+            // Création du Dataframe
+            DataTable dfClient = new DataTable();
+            dfClient.Columns.Add("Numéro de Sécurité Sociale", typeof(string));
+            dfClient.Columns.Add("Nom", typeof(string));
+            dfClient.Columns.Add("Prénom", typeof(string));
+            dfClient.Columns.Add("Date de Naissance", typeof(DateTime));
+            dfClient.Columns.Add("Adresse Postale", typeof(string));
+            dfClient.Columns.Add("Adresse Email", typeof(string));
+            dfClient.Columns.Add("Téléphone", typeof(string));
+            dfClient.Columns.Add("Montant Achats Cumulés", typeof(decimal));
 
+            // Remplissage du Dataframe
+            foreach (Client c in ClientList)
+            {
+                dfClient.Rows.Add(c.NumeroSS,c.Nom,c.Prenom,c.DateNaissance,c.adressePostale,c.adresseMail,c.Telephone,c.montantAchatsCumulés);
+            }
+
+            // Tri par Nom
+            DataView vueParNom = dfClient.DefaultView;
+            vueParNom.Sort = "Nom ASC";
+            DataTable dfClientTrierParNom = vueParNom.ToTable();
+
+            // Tri par Montant
+            DataView vueParMontant = dfClient.DefaultView;
+            vueParMontant.Sort = "Montant Achats Cumulés DESC";
+            DataTable dfClientTrieParMontant = vueParMontant.ToTable();
+
+            return dfClientTrierParNom;
+        }
     }
 }
+
